4746f9c0cfa969f7db9b1ae182de6da2ee51b4a1
Adam Vorkel <adamvorkel@gmail.com>
2020-10-16 17:16:53 +0200

    basic client base styling and header styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Search, User } from 'react-feather';
 
 export const Header = () => (
-    <div>
-        <a href="./">GoodAdvice</a>
+    <header>
+        <a href="./" className="logo">GoodAdvice</a>
         <span><Search size={16} /><input type="search" placeholder="Search..." /></span>
         <a href="./login"><User size={16} /><span>Login</span></a>
-    </div>
+    </header>
 )
