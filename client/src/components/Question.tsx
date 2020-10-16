e2413264b813fa5bb67f19f968f883c78981e02b
Adam Vorkel <adamvorkel@gmail.com>
2020-10-16 19:09:15 +0200

    client question component now shows content by default, can be hidden with showContent prop

--- client/src/components/Question.tsx
+++ client/src/components/Question.tsx
@@ -3,12 +3,14 @@ import React, { FC } from 'react';
 import { QuestionData } from '../QuestionsData';
 
 interface Props {
-    data: QuestionData
+    data: QuestionData,
+    showContent?: boolean
 };
 
-export const Question: FC<Props> = ({ data }) => (
+export const Question: FC<Props> = ({ data, showContent = true }) => (
     <div className="question">
         <h3 className="question-title">{data.title}</h3>
-        <p className="question-created">Asked by {data.userName} on {data.created.toLocaleDateString()} {data.created.toLocaleTimeString()}</p>
+        {showContent && (<p>{data.content}</p>)}
+        <span className="question-created">Asked by {data.userName} on {data.created.toLocaleDateString()} {data.created.toLocaleTimeString()}</span>
     </div>
 )
